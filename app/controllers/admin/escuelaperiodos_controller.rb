e37dfb8b75a8e9f8dc9528cca20cab58d7c70ce3
Daniel Moros <danielousky@gmail.com>
2021-02-09 15:20:27 -0400

    Ajsutes temporales para ocultar vistas de inscripcion

--- app/controllers/admin/escuelaperiodos_controller.rb
+++ app/controllers/admin/escuelaperiodos_controller.rb
@@ -8,7 +8,7 @@ module Admin
 		def show
 			@escupe = Escuelaperiodo.find params[:id]
 
-			if @escupe.escuela_id.eql? 'IDIO' and @escupe.periodo_id.eql? '2019-02A'
+			if false #@escupe.escuela_id.eql? 'IDIO' and @escupe.periodo_id.eql? '2019-02A'
 				if params[:rat]
 					@inscripciones = Inscripcionseccion.joins(:escuela).where("escuelas.id = ?", @escupe.escuela_id).del_periodo(@escupe.periodo_id).inscritos.joins(:usuario).order("usuarios.apellidos ASC").joins(:asignatura).group(:estudiante_id).select('estudiante_id, usuarios.apellidos apellidos, usuarios.nombres nombres, SUM(asignaturas.creditos) total_creditos, COUNT(*) asignaturas, SUM(IF (estado = 1, asignaturas.creditos, 0)) aprobados')
 					@titulo = "Ratificaciones para el período #{@escupe.periodo_id} en la escuela #{@escupe.escuela.descripcion} (#{@inscripciones.size.count})"
