ffa8aa8d886d2a4ebf7ec13062ce51a5be0188d8
Daniel Moros <danielousky@gmail.com>
2021-03-16 19:01:53 -0400

    Bug: Ajustes en el formulario de imágenes y en el controlador de profesores

--- app/controllers/admin/principal_profesor_controller.rb
+++ app/controllers/admin/principal_profesor_controller.rb
@@ -5,9 +5,9 @@ module Admin
 		before_action :filtro_profesor
 
 		def index
-			@usuario = Usuario.find session[:profesor_id]
-			@titulo = "Principal - Profesores"	
 			@profesor = Profesor.find (session[:profesor_id])
+			@usuario = @profesor.usuario
+			@titulo = "Principal - Profesores"	
 			@secciones_pendientes = @profesor.secciones.sin_calificar.order('periodo_id DESC, numero ASC')
 			@secciones_calificadas = @profesor.secciones.calificadas.order('periodo_id DESC, numero ASC')
 			@secciones_secundarias = @profesor.secciones_secundarias.order('periodo_id DESC, numero ASC')#.where(periodo_id: @periodo_actual_id)
