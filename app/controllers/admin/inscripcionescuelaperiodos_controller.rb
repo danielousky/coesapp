8ac573d95168a93843b9ee55111dcffe82525961
Daniel Moros <danielousky@gmail.com>
2022-01-17 16:18:57 -0400

    Update inscripcionescuelaperiodos_controller.rb

--- app/controllers/admin/inscripcionescuelaperiodos_controller.rb
+++ app/controllers/admin/inscripcionescuelaperiodos_controller.rb
@@ -8,19 +8,19 @@ module Admin
 			
 			if @escuelaperiodo = Escuelaperiodo.find(params[:escuelaperiodo_id])
 				@inscripciones = @escuelaperiodo.inscripcionescuelaperiodos
-				@titulo = "Inscripciones para el #{@escuelaperiodo.periodo_id} en #{@escuelaperiodo.escuela.descripcion}"
+				@titulo = "Inscripciones - #{@escuelaperiodo.periodo_id} - #{@escuelaperiodo.escuela.id}"
 				@titulo += " <span class='badge badge-secondary'>#{@inscripciones.count}</span>"
 				if params[:status].eql? TipoEstadoInscripcion::PREINSCRITO
 					@inscripciones = @inscripciones.sin_reportepago.preinscritos
-					@titulo = "Preinscritos Sin Reporte #{@escuelaperiodo.periodo_id} en #{@escuelaperiodo.escuela.descripcion}"
+					@titulo = "Preinscritos Sin Reporte - #{@escuelaperiodo.periodo_id} - #{@escuelaperiodo.escuela.id}"
 					@titulo += " <span class='badge badge-primary'>#{@inscripciones.count}</span>"
 				elsif params[:status].eql? 'reported'
 					@inscripciones = @inscripciones.con_reportepago.preinscritos
-					@titulo = "Preinscritos Con Reporte #{@escuelaperiodo.periodo_id} en #{@escuelaperiodo.escuela.descripcion}"
+					@titulo = "Preinscritos Con Reporte - #{@escuelaperiodo.periodo_id} - #{@escuelaperiodo.escuela.id}"
 					@titulo += " <span class='badge badge-warning'>#{@inscripciones.count}</span>"
 				elsif params[:status].eql? TipoEstadoInscripcion::INSCRITO
 					@inscripciones = @inscripciones.inscritos
-					@titulo = "Preinscritos Confirmados #{@escuelaperiodo.periodo_id} en #{@escuelaperiodo.escuela.descripcion}"
+					@titulo = "Confirmados - #{@escuelaperiodo.periodo_id} - #{@escuelaperiodo.escuela.id}"
 					@titulo += " <span class='badge badge-success'>#{@inscripciones.count}</span>"
 
 				end
